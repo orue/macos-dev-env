46805f881ed8921e07b32a38ff993e87ba89082f
Carlos Orue <carlos@orue.io>
2021-10-31 12:58:50 -0500

    update brew script

--- brew-script.sh
+++ brew-script.sh
@@ -9,18 +9,22 @@ brew tap homebrew/cask
 
 # Github
 brew install git
-brew install --cask github
 brew install gh
 
+# passwor manager
+brew install --cask 1password
 
 # Terminals
 brew install --cask iterm2
 brew install --cask alacritty
 
 # Terminal tools
-brew install bpytop
 brew install curl
 brew install lazygit
+brew install exa
+brew install bat
+brew install ranger
+brew install neofetch
 
 # Fonts
 brew tap homebrew/cask-fonts
@@ -32,37 +36,28 @@ brew install --cask font-hack-nerd-font
 brew install --cask firefox
 
 # Programming Tools
-brew install nvm
-brew install yarn
-brew install pipx
-brew install pyenv
+brew install fnm
+# brew install yarn
 
-
-# VIM Requirement
-brew install ripgrep
 
 # Neovim
 brew install neovim
 
 
 # IDE, Code Editors, Dev-Tools
+echo "Installing IDE, Editors and Dev Tools apps..."
 brew install --cask visual-studio-code
 brew install --cask pycharm
 brew install --cask coteditor
-brew install --cask sublime-text
-brew install --cask atom
-brew install --cask mark-text
 brew install --cask insomnia
-
-# Azure
-brew install azure-cli
+brew install --cask pgadmin4
+brew install neovim
 
 # Alfred
 brew install --cask alfred
 
 # Communication Apps
 echo "Installing communication apps..."
-brew install --cask discord
 brew install --cask microsoft-teams
 brew install --cask slack
 brew install --cask zoom
@@ -72,6 +67,7 @@ echo "Installing file storage tools..."
 brew install --cask dropbox
 
 # Other Apps
+# brew install --cask rectangle
 brew install --cask spotify
 brew install --cask balenaetcher
 brew install --cask authy
