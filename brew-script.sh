f002c42d635db00a78ed610c5b492805fa51c871
Carlos Orue <carlos@orue.io>
2022-07-14 14:55:27 -0500

    Add dropzone to script

--- brew-script.sh
+++ brew-script.sh
@@ -60,7 +60,7 @@ echo "Installing file storage tools..."
 # brew install --cask dropbox
 
 # Other Apps
-# brew install --cask rectangle
+brew install --cask dropzone
 brew install --cask spotify
 brew install --cask balenaetcher
 brew install --cask authy
